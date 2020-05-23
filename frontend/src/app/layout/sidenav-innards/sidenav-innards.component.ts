609c9f8a6885efc3b0ce05e4fea1b62fe4ef1a8a
shastaxc <shastaxc@gmail.com>
2020-05-23 11:59:00 -0400

    Init Commit

---
+++ frontend/src/app/layout/sidenav-innards/sidenav-innards.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dport-sidenav-innards',
+  templateUrl: './sidenav-innards.component.html',
+  styleUrls: ['./sidenav-innards.component.scss'],
+})
+export class SidenavInnardsComponent {}
