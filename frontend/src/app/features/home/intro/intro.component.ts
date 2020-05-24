9e4c0c9b9536f338dd3ffcb449dc7b100ff57ba7
shastaxc <shastaxc@gmail.com>
2020-05-24 12:40:15 -0400

    Updated background. Changed resolution-based styling logic.

--- frontend/src/app/features/home/intro/intro.component.ts
+++ frontend/src/app/features/home/intro/intro.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dport-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
 })
-export class IntroComponent implements OnInit {
-  isMobileDevice: boolean;
-
-  constructor(private deviceService: DeviceDetectorService) {}
-
-  ngOnInit(): void {
-    this.isMobileDevice = this.deviceService.isMobile();
-  }
-}
+export class IntroComponent {}
