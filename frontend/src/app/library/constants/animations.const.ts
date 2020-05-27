4472f316b316625cdd75143473b947fdb3827cba
shastaxc <shastaxc@gmail.com>
2020-05-27 13:35:21 -0400

    Fix repaint performance. Modify route transition animations.

--- frontend/src/app/library/constants/animations.const.ts
+++ frontend/src/app/library/constants/animations.const.ts
@@ -22,8 +22,8 @@ export const slideInAnimation = trigger('routeAnimations', [
     query(':enter', [style({ left: '100%' })]),
     query(':leave', animateChild()),
     group([
-      query(':leave', [animate('500ms ease-out', style({ left: '-100%' }))]),
-      query(':enter', [animate('500ms ease-out', style({ left: '0%' }))]),
+      query(':leave', [animate('300ms ease-out', style({ left: '-100%' }))]),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))]),
     ]),
     query(':enter', animateChild()),
   ]),
@@ -40,8 +40,8 @@ export const slideInAnimation = trigger('routeAnimations', [
     query(':enter', [style({ left: '-100%' })]),
     query(':leave', animateChild()),
     group([
-      query(':leave', [animate('500ms ease-out', style({ left: '100%' }))]),
-      query(':enter', [animate('500ms ease-out', style({ left: '0%' }))]),
+      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))]),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))]),
     ]),
     query(':enter', animateChild()),
   ]),
